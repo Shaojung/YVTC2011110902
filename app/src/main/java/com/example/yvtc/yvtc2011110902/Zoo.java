23fe3c8634d4ff72b50dcf5600e8702132b9dd3f
Shaojung <shaojung@gmail.com>
2017-11-09 14:53:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/yvtc2011110902/Zoo.java
@@ -0,0 +1,9 @@
+package com.example.yvtc.yvtc2011110902;
+
+/**
+ * Created by yvtc on 2017/11/9.
+ */
+
+public class Zoo {
+    public ZooDetail result;
+}
---
+++ app/src/main/java/com/example/yvtc/yvtc2011110902/ZooInfo.java
@@ -0,0 +1,11 @@
+package com.example.yvtc.yvtc2011110902;
+
+/**
+ * Created by yvtc on 2017/11/9.
+ */
+
+public class ZooInfo {
+    public String _id;
+    public String E_Name;
+    public String E_Pic_URL;
+}
